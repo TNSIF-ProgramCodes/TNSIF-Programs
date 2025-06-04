857ed4e7550dc28ca621c93cf2bdb769f4fe563b
TNSIF-ProgramCodes <dileepvp2004@gmail.com>
2025-06-04 11:49:47 +0530

    Initial commit

---
+++ Example.java
@@ -0,0 +1,16 @@
+package packages.demo;
+
+import com.tns.daythree.PackageDemo;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		
+		PackageDemo pd = new PackageDemo();
+		pd.show();
+		
+
+	}
+
+}
